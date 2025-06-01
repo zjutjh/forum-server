a5af0ca835870a364a951a546a699a4b37ea9305
MeaquaOWO <189289022@qq.com>
2025-06-01 15:13:18 +0800

    feat:新增用户个人页面DTO&Controller

--- forum_user/src/main/java/org/jh/forum/user/UserApplication.java
+++ forum_user/src/main/java/org/jh/forum/user/UserApplication.java
@@ -3,9 +3,13 @@ package org.jh.forum.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 用户模块启动类
+ * 扫描用户模块包路径 [[4.2.2]]
+ */
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(UserApplication.class, args); // 启动模块
     }
 }
