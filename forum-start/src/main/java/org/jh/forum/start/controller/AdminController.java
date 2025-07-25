5f99467ce7521694b301bab17086f0973b4b346d
MeaquaOWO <189289022@qq.com>
2025-07-25 18:20:25 +0800

    feat:新增禁言接口

--- forum-start/src/main/java/org/jh/forum/start/controller/AdminController.java
+++ forum-start/src/main/java/org/jh/forum/start/controller/AdminController.java
@@ -4,13 +4,22 @@ import cn.dev33.satoken.annotation.SaCheckLogin;
 import cn.dev33.satoken.annotation.SaCheckRole;
 import cn.dev33.satoken.annotation.SaMode;
 import cn.dev33.satoken.stp.StpUtil;
+import com.google.protobuf.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.jh.forum.api.dubbo.service.UserService;
+import org.jh.forum.common.constants.ExceptionEnum;
+import org.jh.forum.common.dto.UserDTO;
+import org.jh.forum.common.dto.request.MuteRequest;
+import org.jh.forum.common.dto.request.UserUpdateAdminRequest;
+import org.jh.forum.common.dto.response.UserDetailResponse;
+import org.jh.forum.common.entity.User;
+import org.jh.forum.common.exceptions.ApiException;
+import org.jh.forum.server.mapper.UserMapper;
 import org.jh.forum.start.models.AjaxResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +42,63 @@ public class AdminController {
         // 获取当前登录用户角色
         List<String> roleList = StpUtil.getRoleList();
         log.info("角色权限列表 {}", roleList);
+        return AjaxResult.success();
+    }
+
+    /**
+     * @author MeaquaOWO
+     */
+    @Autowired
+    private UserService userService;
+
+    // 禁言/解禁用户
+    @PutMapping("/muted")
+    public AjaxResult<Void> updateMuteStatus(@RequestBody MuteRequest dto) {
+        Long userId= Long.parseLong(dto.getUserId());
+        try {
+            userService.updateMuteStatus(userId, dto.getMutedUntil());
+            return AjaxResult.success();
+        } catch (ApiException e) {
+            return AjaxResult.fail(ExceptionEnum.RESOURCE_NOT_FOUND);
+        } catch (Exception e) {
+            return AjaxResult.fail(ExceptionEnum.SERVER_ERROR);
+        }
+    }
+
+    // 查询禁言状态
+    @GetMapping("/userId/muted")
+    public AjaxResult<Boolean> getMuteStatus(@RequestParam(name="userId",required=false) String targetUserId) {
+        Long userId= Long.parseLong(targetUserId);
+        try {
+            boolean isMuted = userService.isUserMuted(userId);
+            return AjaxResult.success(isMuted);
+        } catch (ApiException e) {
+            return AjaxResult.fail(ExceptionEnum.RESOURCE_NOT_FOUND);
+        } catch (Exception e) {
+            return AjaxResult.fail(ExceptionEnum.SERVER_ERROR);
+        }
+    }
+
+    // 管理员获取用户资料
+    @GetMapping("/users/userId")
+    public AjaxResult<UserDetailResponse> getProfileByAdmin(@RequestParam(name="userId",required=false) String targetUserId) {
+        Long userId= Long.parseLong(targetUserId);
+        Long currentUserId = StpUtil.getLoginIdAsLong();
+        if (userId.equals(currentUserId)) {
+            UserDTO targetUser = userService.getUserById(userId);
+            return AjaxResult.success(userService.filterFields(targetUser, userId));
+        } else {
+            UserDTO targetUser = userService.getUserById(userId);
+            return AjaxResult.success(userService.filterFields(targetUser, userId));
+        }
+    }
+
+    // 管理员更新用户资料
+    @PutMapping("/users")
+    //@SaCheckRole("ADMIN")
+    public AjaxResult<Void> updateProfileByAdmin(@RequestBody UserUpdateAdminRequest dto) {
+        Long userId= Long.parseLong(dto.getUserId());
+        userService.updateUserProfileByAdmin(userId, dto);
         return AjaxResult.success();
     }
 }
